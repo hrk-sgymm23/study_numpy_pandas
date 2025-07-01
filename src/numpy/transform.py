704f5b76356da1bca2ece92db96b6da6f547af37
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2025-07-02 00:09:04 +0900

    feat: add flip

--- src/numpy/transform.py
+++ src/numpy/transform.py
@@ -7,3 +7,10 @@ print(data.reshape(3, 2))
 
 arr = np.arange(6).reshape((2, 3))
 print(arr)
+
+print(arr.transpose())  
+
+arr2 = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+reversed_arr = np.flip(arr2)
+
+print(reversed_arr)
