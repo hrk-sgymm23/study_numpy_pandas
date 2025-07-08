d6cb57e2d2f5372fb35a77fe0815eb13c9fbff21
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2025-07-08 21:17:41 +0900

    feat: add transform

--- src/numpy/transform.py
+++ src/numpy/transform.py
@@ -14,3 +14,16 @@ arr2 = np.array([1, 2, 3, 4, 5, 6, 7, 8])
 reversed_arr = np.flip(arr2)
 
 print(reversed_arr)
+
+arr_2d = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+reversed_arr = np.flip(arr_2d)
+print(reversed_arr)
+
+reversed_arr_columns = np.flip(arr_2d, axis=1)
+print(reversed_arr_columns)
+
+arr_2d[1] = np.flip(arr_2d[1])
+print(arr_2d)
+
+arr_2d[:,1] = np.flip(arr_2d[:,1])
+print(arr_2d)
